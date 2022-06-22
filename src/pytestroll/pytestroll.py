e16dcd0d55f0d60b161f0db87e024153a23fddb7
Leo Guelman <leo.guelman@gmail.com>
2022-06-21 20:01:03 -0400

    docs: added docstrings

--- src/pytestroll/pytestroll.py
+++ src/pytestroll/pytestroll.py
@@ -158,16 +158,20 @@ class TestRoll:
     def profit_nn(self, n, log_n:bool = False, sign:float = 1.0):
         """
         Computes the per-customer profit for test & roll with 2 arms
-        where response is normal with (possibly assymetric) normal priors 
+        where response is normal with (possibly asymetric) normal priors 
         
         Parameters
         ----------
         n: array-like 
            Sample sizes
         
-        log_n: bool (TBD)
+        log_n: bool, optional 
+            Internally set to `True` when profit-maximizing test size 
+            with asymetric priors.
 
-        sign: float(TBD)
+        sign: float, optional
+            Internally set to `-1` when profit-maximizing test size 
+            with asymetric priors.
          
         Returns
         -------
@@ -195,73 +199,85 @@ class TestRoll:
             
         return res
     
-    def _one_rep_profit(self, n:float = None, K:int = None, TS:bool = False, seed:int = None):
-        
-         np.random.seed(seed)
-         
-         # utility function used in profit_nn_sim() to simulate one set of potential outcomes
-         m = np.random.normal(self.mu, self.sigma, size=K)    
-         y = np.random.normal(m, self.s, size=(self.N,K))
-         
-         # perfect information profit
-         perfect_info = sum(y[:,np.argmax(m)])
-         postmean = np.empty(2)
-         
-         # test and roll profit with sample sizes n
-         # Normal-Normal model https://stephens999.github.io/fiveMinuteStats/shiny_normal_example.html
-         for k in range(K):
-             postvar = 1/(1/self.sigma[k]**2 + n[k]/self.s[k]**2)
-             postmean[k] = postvar*(self.mu[k]/self.sigma[k]**2 + sum(y[0:int(np.floor(n[k])), k])/self.s[k]**2)
-      
-         delta = np.argmax(postmean) # pick the arm with the highest posterior mean
-         error = delta != np.argmax(m)
-         deploy_1 = delta == 0
-         test_roll = 0
-         for k in range(K):
-             test_roll = test_roll + sum(y[0:int(np.floor(n[k])), k]) # profit from first n observations for each arm
-         test_roll = test_roll + sum(y[int(np.floor((sum(n)))):self.N, delta]) # profit from remaining observations for selected arm
-         
-         # Thompson sampling profit
-         thom_samp = None
-         if TS:
-             n = np.repeat(0, K) # Initialize each arm with zero observations 
-             # note mu and sigma are initialized at the priors
-             postvar = (1/(np.expand_dims(1/self.sigma**2, 1) + (np.repeat(np.arange(self.N), K).reshape((self.N, K)) + 1).T * np.expand_dims(1/self.s**2, 1))).T
-             postmean = postvar * ((np.expand_dims(self.mu/self.sigma**2, 1) + (np.apply_along_axis(np.cumsum, 0, y)).T * np.expand_dims(1/self.s**2, 1))).T
-
-             mu = self.mu
-             sigma = self.sigma
-            
-             for i in range(self.N):
-                 k = np.argmax(np.random.normal(mu, sigma, size=K))
-                 mu[k] = postmean[n[k], k] # advance mu and sigma 
-                 sigma[k] = np.sqrt(postvar[n[k], k])
-                 n[k] =n[k]+ 1 # increase the number of observations used from sampled arm
-             thom_samp = 0
-             for k in range(K): 
-                 thom_samp = thom_samp + sum(y[:n[k], k])
-         #return {"perfect_info":perfect_info, "test_roll":test_roll, "thom_samp":thom_samp, 
-         #            "error" : error, "deploy_1":deploy_1}
-         return perfect_info, test_roll, thom_samp, error, deploy_1
-
-
-    def profit_nn_sim(self, n:float = None, K:int = 2, TS:bool = False, R:int = 1000, seed:int = 42):
+    def _one_rep_profit(self, n:float, K:int, TS:bool = False, seed:int = None):
         """
+        Internal function used in profit_nn_sim() to simulate one set of potential outcomes
 
+        """
+        
+        np.random.seed(seed)
+        
+        # utility function used in profit_nn_sim() to simulate one set of potential outcomes
+        m = np.random.normal(self.mu, self.sigma, size=K)    
+        y = np.random.normal(m, self.s, size=(self.N,K))
+        
+        # perfect information profit
+        perfect_info = sum(y[:,np.argmax(m)])
+        postmean = np.empty(2)
+        
+        # test and roll profit with sample sizes n
+        # Normal-Normal model https://stephens999.github.io/fiveMinuteStats/shiny_normal_example.html
+        for k in range(K):
+            postvar = 1/(1/self.sigma[k]**2 + n[k]/self.s[k]**2)
+            postmean[k] = postvar*(self.mu[k]/self.sigma[k]**2 + sum(y[0:int(np.floor(n[k])), k])/self.s[k]**2)
+     
+        delta = np.argmax(postmean) # pick the arm with the highest posterior mean
+        error = delta != np.argmax(m)
+        deploy_1 = delta == 0
+        test_roll = 0
+        for k in range(K):
+            test_roll = test_roll + sum(y[0:int(np.floor(n[k])), k]) # profit from first n observations for each arm
+        test_roll = test_roll + sum(y[int(np.floor((sum(n)))):self.N, delta]) # profit from remaining observations for selected arm
+        
+        # Thompson sampling profit
+        thom_samp = None
+        if TS:
+            n = np.repeat(0, K) # Initialize each arm with zero observations 
+            # note mu and sigma are initialized at the priors
+            postvar = (1/(np.expand_dims(1/self.sigma**2, 1) + (np.repeat(np.arange(self.N), K).reshape((self.N, K)) + 1).T * np.expand_dims(1/self.s**2, 1))).T
+            postmean = postvar * ((np.expand_dims(self.mu/self.sigma**2, 1) + (np.apply_along_axis(np.cumsum, 0, y)).T * np.expand_dims(1/self.s**2, 1))).T
+
+            mu = self.mu
+            sigma = self.sigma
+           
+            for i in range(self.N):
+                k = np.argmax(np.random.normal(mu, sigma, size=K))
+                mu[k] = postmean[n[k], k] # advance mu and sigma 
+                sigma[k] = np.sqrt(postvar[n[k], k])
+                n[k] =n[k]+ 1 # increase the number of observations used from sampled arm
+            thom_samp = 0
+            for k in range(K): 
+                thom_samp = thom_samp + sum(y[:n[k], k])
+        #return {"perfect_info":perfect_info, "test_roll":test_roll, "thom_samp":thom_samp, 
+        #            "error" : error, "deploy_1":deploy_1}
+        return perfect_info, test_roll, thom_samp, error, deploy_1
+
+
+    def profit_nn_sim(self, n:float, K:int = 2, TS:bool = False, R:int = 1000, seed:int = 42):
+        """
+        Computes the per-customer profit for test & roll with K arms
+        
         Parameters
         ----------
-        n : TYPE
-            DESCRIPTION.
-       K : TYPE, optional
-            DESCRIPTION. The default is 2.
-        TS : TYPE, optional
-            DESCRIPTION. The default is FALSE.
-        R : TYPE, optional
-            DESCRIPTION. The default is 1000.
-
+        n: array-like 
+           Sample sizes
+        
+        K: int
+           Number of arms
+        
+        TS: bool, optional
+           Whether or not to run Thomson sampling
+        
+        R: int, optional
+           Number of simulation replications
+        
+        seed: int, optional
+            Random seed
+         
         Returns
         -------
-        None.
+        Dictionary
+            Containing the profit, regret, and error rates.
 
         """
         
@@ -310,6 +326,10 @@ class TestRoll:
     
     
     def _profit_perfect_nn(self):
+        """
+        Internal function used to compute per-customer profit with perfect information
+        where response is normal with symmetric normal priors
+        """
 
         res = self.mu + self.sigma/np.sqrt(np.pi)   #Eq. 13
         
@@ -317,7 +337,11 @@ class TestRoll:
 
 
 
-    def _error_rate_nn(self, n:float = None):
+    def _error_rate_nn(self, n:float):
+        """
+        Internal function used to compute the rate of incorrect deployments when response 
+        is normal with symmetric normal priors
+        """
         
         if self.s.size != 1:   
             raise ValueError("Method only applicable to symmetric priors.")
@@ -328,62 +352,72 @@ class TestRoll:
 
     
     
-    def eval_nn(self, n:float = None, R:int = None):
+    def eval_nn(self, n:float, R:int=1000):
+        """
+        Provides a complete summary of a test & roll plan
         
-         profit = self.profit_nn(n)*self.N
-     
-         if self.s.size == 1:
-             test = self.mu * (n[0] + n[1]) 
-             deploy = profit - test
-             rand = self.mu * self.N # choose randomly
-             perfect = self._profit_perfect_nn() * self.N 
-             error_rate = self._error_rate_nn(n)
-             deploy_1_rate = 0.5
+        Parameters
+        ----------
+        n: array-like 
+           Sample sizes
+        
+        R: int
+           Number of simulation replications. Argument is passed to `profit_nn_sim`
+           in the assymetric priors case
+           
+        Returns
+        -------
+        Dictionary
+            Containing summary statistics such as profit per customer,
+            profits from test phase, error rates, etc.
+
+        """
+        
+        profit = self.profit_nn(n)*self.N
+    
+        if self.s.size == 1:
+            test = self.mu * (n[0] + n[1]) 
+            deploy = profit - test
+            rand = self.mu * self.N # choose randomly
+            perfect = self._profit_perfect_nn() * self.N 
+            error_rate = self._error_rate_nn(n)
+            deploy_1_rate = 0.5
+           
+        else:
+            test = self.mu[0] * n[0] + self.mu[1] *n[1] 
+            deploy = profit - test
+            rand = ((self.mu[0] + self.mu[1])*0.5)*self.N
+            out = self.profit_nn_sim(n, R=R)
+            perfect = out['profit'].iloc[0,1]*self.N
+            error_rate = out['error_rate']
+            deploy_1_rate =  out['deploy_1_rate']
             
-         else:
-             test = self.mu[0] * n[0] + self.mu[1] *n[1] 
-             deploy = profit - test
-             rand = ((self.mu[0] + self.mu[1])*0.5)*self.N
-             out = self.profit_nn_sim(n, R=R)
-             perfect = out['profit'].iloc[0,1]*self.N
-             error_rate = out['error_rate']
-             deploy_1_rate =  out['deploy_1_rate']
-             
-         gain = (profit - rand) / (perfect - rand)
+        gain = (profit - rand) / (perfect - rand)
+        
+        data = {
+             'n1':n[0], 
+             'n2':n[1],
+             'profit_per_cust' : profit/self.N,
+             'profit': profit, 
+             'profit_test' : test, 
+             'profit_deploy' : deploy,
+             'profit_rand' : rand,
+             'profit_perfect' : perfect, 
+             'profit_gain' : gain, 
+             'regret' : 1-profit/perfect,
+             'error_rate' : error_rate, 
+             'deploy_1_rate' : deploy_1_rate, 
+             'tie_rate' : 0.0
+            }
+        return data 
+           
+     
+    def _one_rep_test_size(self, n_vals:float, K:int = 2, seed:int = 42):
+         """
+         Internal function used in `tr_size_nn_sim` to simulate one set of potential outcomes
+         and profits for all possible equal sample sizes
+         """
          
-         data = {
-              'n1':n[0], 
-              'n2':n[1],
-              'profit_per_cust' : profit/self.N,
-              'profit': profit, 
-              'profit_test' : test, 
-              'profit_deploy' : deploy,
-              'profit_rand' : rand,
-              'profit_perfect' : perfect, 
-              'profit_gain' : gain, 
-              'regret' : 1-profit/perfect,
-              'error_rate' : error_rate, 
-              'deploy_1_rate' : deploy_1_rate, 
-              'tie_rate' : 0.0
-             }
-         return data 
-            
-     
-    def _one_rep_test_size(self, n_vals:float = None, K:int = None, seed:int = None):
-         """
-
-         Parameters
-         ----------
-         n_vals : TYPE
-             DESCRIPTION.
-        K : TYPE
-             DESCRIPTION.
-
-         Returns
-         -------
-         None.
-
-         """
          np.random.seed(seed)
          
          m = np.random.normal(self.mu, self.sigma, size=K)  
@@ -406,43 +440,64 @@ class TestRoll:
 
 
     def tr_size_nn_sim(self, K:int = 2, R:int = 1000, seed:int = 42):
-         
-         np.random.seed(seed)
-         r = [np.random.randint(1,10000) for _ in range(R)]
-         
-         s = self.s
-         mu = self.mu
-         sigma = self.sigma
+        """
+        Computes the profit-maximizing test size for a multi-armed test & roll
+        where response is normal with normal priors (possibly asymmetric)
+        
+        Parameters
+        ----------
+        K: int
+            Number of arms
+        
+        R: int
+            Number of simulation replications
            
-         if s.size == 1:   
+        seed: int, optional
+            Random seed
+           
+        Returns
+        -------
+        Dictionary
+            Containing sample sizes and expected profit per customer
+
+        """
          
-             self.s = np.repeat(s, K)
-             self.mu = np.repeat(mu, K)
-             self.sigma = np.repeat(sigma, K)
-         
-             n_vals = np.array(range(int(np.floor(self.N/K)-1)))
-         
-             n_vals_, profit_ = zip(*Parallel(n_jobs=-2)(delayed(self._one_rep_test_size)(n_vals, K, seed=r[i]) for i in range(R)))
-             reps = pd.DataFrame({'n_vals': np.hstack(n_vals_), 'profit': np.hstack(profit_)})
-             exp_profit = pd.pivot_table(reps, values='profit', columns = 'n_vals', aggfunc = 'sum') / R
-             n = np.repeat(n_vals[np.argmax(exp_profit)], K)
-             max_exp_profit = np.max(exp_profit.values)/self.N
-         
-         else:
-             raise ValueError("Asymmetric test not implemented yet.")
-             
-             #n = None
-             # todo:finish this
-             # best option is to use a numeric optimization, but then noise in profit_nn_sim
-             # becomes critical. comments out is one strategy
-             # start values based on two-arm symmetric
-             #n <- ( -3 * s[1]^2 + sqrt( 9*s[1]^4 + 4*N*s[1]^2*sigma[1]^2 ) ) / (4 * sigma[1]^2)
-             #n <- optim(par=log(n), fn=profit_nn_sim, control=list(fnscale=-1), 
-             #           N=N, s=s, mu=mu, sigma=sigma, K=K, R=1000, log_n=TRUE)$par
-             #n <- optim(par=n, fn=profit_nn_sim, control=list(fnscale=-1),  # more precise simulation
-             #           N=N, s=s, mu=mu, sigma=sigma, K=K, R=5000, log_n=TRUE)$par
-             
-         return {'n':n, 'max_exp_profit':max_exp_profit}
-  
-     
+        np.random.seed(seed)
+        r = [np.random.randint(1,10000) for _ in range(R)]
+        
+        s = self.s
+        mu = self.mu
+        sigma = self.sigma
+          
+        if s.size == 1:   
+        
+            self.s = np.repeat(s, K)
+            self.mu = np.repeat(mu, K)
+            self.sigma = np.repeat(sigma, K)
+        
+            n_vals = np.array(range(int(np.floor(self.N/K)-1)))
+        
+            n_vals_, profit_ = zip(*Parallel(n_jobs=-2)(delayed(self._one_rep_test_size)(n_vals, K, seed=r[i]) for i in range(R)))
+            reps = pd.DataFrame({'n_vals': np.hstack(n_vals_), 'profit': np.hstack(profit_)})
+            exp_profit = pd.pivot_table(reps, values='profit', columns = 'n_vals', aggfunc = 'sum') / R
+            n = np.repeat(n_vals[np.argmax(exp_profit)], K)
+            max_exp_profit = np.max(exp_profit.values)/self.N
+        
+        else:
+            raise ValueError("Asymmetric test not implemented yet.")
+            
+            #n = None
+            # todo:finish this
+            # best option is to use a numeric optimization, but then noise in profit_nn_sim
+            # becomes critical. comments out is one strategy
+            # start values based on two-arm symmetric
+            #n <- ( -3 * s[1]^2 + sqrt( 9*s[1]^4 + 4*N*s[1]^2*sigma[1]^2 ) ) / (4 * sigma[1]^2)
+            #n <- optim(par=log(n), fn=profit_nn_sim, control=list(fnscale=-1), 
+            #           N=N, s=s, mu=mu, sigma=sigma, K=K, R=1000, log_n=TRUE)$par
+            #n <- optim(par=n, fn=profit_nn_sim, control=list(fnscale=-1),  # more precise simulation
+            #           N=N, s=s, mu=mu, sigma=sigma, K=K, R=5000, log_n=TRUE)$par
+            
+        return {'n':n, 'max_exp_profit':max_exp_profit}
+ 
+    
             
